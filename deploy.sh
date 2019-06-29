3b5d94cd726a3653000538a1d31ac4b94ebe9e81
Alex <awh4kc@virginia.edu>
2019-06-29 16:40:39 -0400

    updating site Sat 29 Jun 2019 04:40:39 PM EDT

--- deploy.sh
+++ deploy.sh
@@ -22,3 +22,17 @@ git push origin master
 
 # Come Back up to the Project Root
 cd ..
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="updating site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Come Back up to the Project Root
+cd ..
+
